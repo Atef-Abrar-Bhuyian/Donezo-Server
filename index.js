52ec97741066788cdc17da52d3d36fe45f4316f6
Atef-Abrar-Bhuyian <abrarbhuyian8@gmail.com>
2025-02-20 19:02:58 +0600

    user collection add to mongodb

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
+// middleware
+app.use(cors());
+app.use(express.json());
+
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.68dnu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -21,6 +25,28 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const userCollection = client.db("donezoDB").collection("users");
+
+    app.get("/users", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
+    // users collection
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
+
+      if (existingUser) {
+        return res.send({ message: "User Already Exists", insertedId: null });
+      }
+
+      const result = await userCollection.insertOne(user);
+
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
