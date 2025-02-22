72fad9e80abb8cf20907cedf8c9ef376e857c812
Atef-Abrar-Bhuyian <abrarbhuyian8@gmail.com>
2025-02-22 19:57:53 +0600

    middleware express.urlencoded added

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded());
 
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
